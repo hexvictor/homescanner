825013a01e3c251557743e1138f7355eada82a8a
Victor Oliveira <jvictorddo@gmail.com>
2025-03-14 19:13:17 +0000

    feat: add format property data function

--- app/actions/addProperty.ts
+++ app/actions/addProperty.ts
@@ -1,5 +1,38 @@
 'use server';
 
-export default async function addProperty() {
-  console.log('Added');
+export default async function addProperty(formData: FormData) {
+  const amenities = formData.getAll('amenities');
+  const images = formData
+    .getAll('images')
+    .filter(image => image instanceof File && image.name !== '')
+    .map(image => image instanceof File && image.name);
+
+  const propertyData = {
+    type: formData.get('type'),
+    name: formData.get('name'),
+    description: formData.get('description'),
+    location: {
+      street: formData.get('location.street'),
+      city: formData.get('location.city'),
+      state: formData.get('location.state'),
+      zipcode: formData.get('location.zipcode'),
+    },
+    beds: formData.get('beds'),
+    baths: formData.get('baths'),
+    square_feet: formData.get('square_feet'),
+    amenities,
+    rates: {
+      nightly: formData.get('rates.nightly'),
+      weekly: formData.get('rates.weekly'),
+      monthly: formData.get('rates.monthly'),
+    },
+    seller_info: {
+      name: formData.get('seller_info.name'),
+      email: formData.get('seller_info.email'),
+      phone: formData.get('seller_info.phone'),
+    },
+    images,
+  };
+
+  console.log(propertyData);
 }
--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,9 @@ import { getToken } from 'next-auth/jwt';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-  console.log('Middleware triggered:', req.nextUrl.pathname); // Debugging
-
   const token = await getToken({ req });
 
   if (!token) {
-    console.log('Test Token:', token); // Debugging
     return NextResponse.rewrite(new URL('/unauthorized', req.nextUrl.origin));
   }
 
